4b92a6ace6de81bc76070d3fe22372bb2ad1a462
amomin2004 <alimomin2004@yahoo.com>
2025-07-18 15:59:16 -0400

    Trained & Evaluvated Models

--- src/model/train_gradient_boost.py
+++ src/model/train_gradient_boost.py
@@ -1,19 +1,18 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import joblib
 
 
+# Load data
 XTrain = pd.read_csv('data/processed/X_train.csv')
 yTrain = pd.read_csv('data/processed/y_train.csv')
-
-
 XTest = pd.read_csv('data/processed/X_test.csv')
 yTest = pd.read_csv('data/processed/y_test.csv')
 
 
-# Initalize
-model = LinearRegression()
+# Initialize the model
+model = GradientBoostingRegressor(random_state=42, n_estimators=100, learning_rate=0.1)
 
 
 # Train
@@ -29,3 +28,7 @@ mse = mean_squared_error(yTest, yPred)
 mae = mean_absolute_error(yTest, yPred)
 r2 = r2_score(yTest, yPred)
 
+
+print(mse)
+print(mae)
+print(r2)
--- src/model/train_random_forest.py
+++ src/model/train_random_forest.py
@@ -1,7 +1,9 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import joblib
+
+
 
 
 XTrain = pd.read_csv('data/processed/X_train.csv')
@@ -12,8 +14,8 @@ XTest = pd.read_csv('data/processed/X_test.csv')
 yTest = pd.read_csv('data/processed/y_test.csv')
 
 
-# Initalize
-model = LinearRegression()
+#Initalize
+model = RandomForestRegressor(random_state=42)
 
 
 # Train
